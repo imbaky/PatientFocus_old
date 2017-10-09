9e3f41e8b1533dc2c2edd4671f92d896ff6d4b7d
Darrel Guerrero <ddayy.guerrero@hotmail.com>
2017-10-09 09:35:51 -0400

    #51 create login component form and  directive

--- client/src/modules/auth/login/containers/login/login.component.ts
+++ client/src/modules/auth/login/containers/login/login.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { Link } from '../../../../core/ui/components/form-group/form-group.component';
+import { FormsValidators } from '../../../../core/ui/validators/forms/forms.validators';
 
 @Component({
   selector: 'login',
   styleUrls: ['./login.component.scss'],
   templateUrl: './login.component.html'
 })
-export class LoginComponent{
-  constructor() { }
+export class LoginComponent {
 
-  ngOnInit() { }
+  forgotPassword: Link = { href: '/', message: 'Forgot password?' };
+
+  form: FormGroup = this.fb.group({
+    'email': ['', [Validators.required, Validators.email ]],
+    'password': ['', Validators.required ],
+    'remain_signed_in': [false, FormsValidators.isTrue() ]
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
 }

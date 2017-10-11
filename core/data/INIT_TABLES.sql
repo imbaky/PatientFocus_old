62eb5f8b1ae932df8665c9241aac2ab7cec78842
McGiver- <gkolampas@gmail.com>
2017-10-11 18:34:39 -0400

    added the patient info struct and start of sql model

--- core/data/INIT_TABLES.sql
+++ core/data/INIT_TABLES.sql
@@ -1,9 +1,15 @@
-CREATE TABLE IF NOT EXISTS patient (
+CREATE TABLE IF NOT EXISTS user (
        id SERIAL NOT NULL PRIMARY KEY,
        fname TEXT NOT NULL,
        lname TEXT NOT NULL,
        address TEXT NOT NULL,
        email TEXT NOT NULL,
+       date_created DATE NOT NULL default CURRENT_DATE,
+       date_modified DATE
+);
+
+CREATE TABLE IF NOT EXISTS patient {
+       id SERIAL NOT NULL PRIMARY KEY,
        race TEXT NOT NULL,
        gender TEXT NOT NULL,
        dob TEXT NOT NULL,
@@ -11,4 +17,4 @@ CREATE TABLE IF NOT EXISTS patient (
        smoke BOOLEAN NOT NULL,
        date_created DATE NOT NULL default CURRENT_DATE,
        date_modified DATE
-);
+}

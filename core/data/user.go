df6b0d20db36bffcb87bd4d06c6c8f26376929fb
Christopher Yap <christopher.yap@ubisoft.com>
2017-10-12 10:54:47 -0400

    Modified SaveUser code to save user to postgres. #59 Fix some syntax problems. Fix missing password field.

--- core/data/user.go
+++ core/data/user.go
@@ -2,27 +2,29 @@ package data
 
 import(
 	"fmt"
-	"encoding/gob"
-	"bytes"
-	"errors"
-	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/imbaky/PatientFocus/core/domain/model"
 )
 
 //Save registers the user
 func SaveUser(user *model.User) error {
-	db, err := leveldb.OpenFile("./data/levelDB", nil)
+	db := GetConnection()
+	if db == nil {
+		return nil
+	}
+
+	// for debug
+	fmt.Println(user)
+
+	var userid int
+	err := db.QueryRow(`INSERT INTO pfuser (fname, lname, password, email)
+	             VALUES ($1, $2, $3, $4) RETURNING id;`, user.FirstName, user.LastName, user.Password, user.Email).Scan(&userid)
 	if err != nil {
+		fmt.Println(err)
 		return err
 	}
-	defer db.Close()
-	var data bytes.Buffer
-	enc := gob.NewEncoder(&data)
-	enc.Encode(*user)
 
-	err = db.Put([]byte(user.Email), data.Bytes(),nil)
-	if err != nil {
-		return errors.New(fmt.Sprintf("could not save user: %v",err))
-	}
-	return nil
+	// should log userid
+
+
+	return err
 }

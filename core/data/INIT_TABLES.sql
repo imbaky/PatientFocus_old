fcf859dc1088871b421aad910e8b339938bd944c
McGyver- <gkolampas@gmail.com>
2017-10-11 21:39:37 -0400

    Patient info struct (#77)
    * #52 added the patient info struct and start of sql model
    
    * #56 the model matches the struct
    
    * Update INIT_TABLES.sql

--- core/data/INIT_TABLES.sql
+++ core/data/INIT_TABLES.sql
@@ -1,14 +1,24 @@
-CREATE TABLE IF NOT EXISTS patient (
+CREATE TABLE IF NOT EXISTS user (
        id SERIAL NOT NULL PRIMARY KEY,
        fname TEXT NOT NULL,
        lname TEXT NOT NULL,
        address TEXT NOT NULL,
        email TEXT NOT NULL,
-       race TEXT NOT NULL,
-       gender TEXT NOT NULL,
-       dob TEXT NOT NULL,
-       language TEXT NOT NULL,
-       smoke BOOLEAN NOT NULL,
        date_created DATE NOT NULL default CURRENT_DATE,
        date_modified DATE
 );
+
+CREATE TABLE IF NOT EXISTS patient {
+       id SERIAL NOT NULL PRIMARY KEY,
+       user INTEGER REFERENCES user(id),
+       race TEXT NOT NULL,
+       gender TEXT NOT NULL,
+       dob TIMESTAMP NOT NULL,
+       language TEXT NOT NULL,
+       smoke BOOLEAN NOT NULL,
+       problem_list TEXT[],
+       meds_list TEXT[],
+       allergy_list TEXT[],
+       date_created DATE NOT NULL default CURRENT_DATE,
+       date_modified clock_timestamp()
+}
---
+++ core/domain/model/patient.go
@@ -0,0 +1,34 @@
+package model
+
+import (
+	"time"
+)
+
+//The Patient struct holds all basic patient info
+type Patient struct {
+	Race        string    `json:"race"`
+	Gender      Gender    `json:"gender"`
+	Language    Language  `json:"language"`
+	DateOfBirth time.Time `json:"dod,string"`
+	Smoke       bool      `json:"smoke"`
+	ProblemList []string  `json:"problem_list"`
+	MedsList    []string  `json:"meds_list"`
+	AlergyList  []string  `json:"alergy_list"`
+}
+
+// Gender is an enum of type string that is either male or female
+type Gender string
+
+// Language is an enum of type string that is either french or english
+type Language string
+
+const (
+	//Male is a string enum that is part of the Gender enum
+	Male Gender = "male"
+	//Female is a string enum that is part of the Gender enum
+	Female Gender = "female"
+	//English is a string enum that is part of the Language enum
+	English Language = "english"
+	//French is a string enum that is part of the Language enum
+	French Language = "french"
+)

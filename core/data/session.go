23063bb501ba298c09e9cc920fb7dbaac45520d8
McGiver- <gkolampas@gmail.com>
2017-10-12 21:47:46 -0400

    Added jwt session token

---
+++ core/data/session.go
@@ -0,0 +1,40 @@
+package data
+
+import (
+	"fmt"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/imbaky/PatientFocus/core/domain/model"
+)
+
+//GetSession creates a session token from the id of the passed user
+func GetSession(user *model.User) (string, error) {
+	mySigningKey := []byte("alrkkjfdvouihregtlkjhiuwer")
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["id"] = user.ID
+	claims["exp"] = time.Now().Add(time.Hour * 2).Unix()
+	tokenString, err := token.SignedString(mySigningKey)
+	return tokenString, err
+}
+
+// CheckSession validates the session token
+func CheckSession(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte("alrkkjfdvouihregtlkjhiuwer"), nil
+	})
+	if err != nil {
+		return fmt.Errorf("could not parse session token %v", err)
+	}
+
+	_, ok := token.Claims.(jwt.MapClaims)
+	if !ok || token.Valid {
+		return fmt.Errorf("session token not ok or not valid")
+	}
+	return nil
+}
--- core/domain/model/user.go
+++ core/domain/model/user.go
@@ -2,9 +2,10 @@ package model
 
 //The User struct holds all user data
 type User struct {
-	Email            string  `json:"email"`
-	FirstName        string  `json:"first_name"`
-	LastName         string  `json:"last_name"`
-	Role			 string  `json:"role"`
-	Password         string  `json:"password"`
+	ID        string `json:"id"`
+	Email     string `json:"email"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Role      string `json:"role"`
+	Password  string `json:"password"`
 }

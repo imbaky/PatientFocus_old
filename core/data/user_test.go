a1d2bf8c05a5bfbbaee86a69afb04bb64f15014c
McGyver- <gkolampas@gmail.com>
2017-10-18 19:06:43 -0400

    Refactoring testing coverage (#88)
    * added some test and code coverage reports
    
    * Changed signing key to a constant.
    
    * Update user_test.go

---
+++ core/data/user_test.go
@@ -0,0 +1,70 @@
+package data
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/imbaky/PatientFocus/core/domain/model"
+)
+
+func TestGetUser(t *testing.T) {
+	type args struct {
+		userid string
+	}
+	patient := &model.Patient{}
+	tests := []struct {
+		name     string
+		args     args
+		wantUser model.User
+		wantErr  bool
+	}{
+		{"getUser1", args{"111"}, model.User{-1, "test@gmail.com", "tester", "testerson", "patient", "password", patient}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotUser := GetUser(tt.args.userid); !tt.wantErr && !reflect.DeepEqual(gotUser, tt.wantUser) {
+				t.Errorf("GetUser() = %v, want %v", gotUser, tt.wantUser)
+			}
+		})
+	}
+}
+
+func TestSaveUser(t *testing.T) {
+	type args struct {
+		user *model.User
+	}
+	patient := &model.Patient{}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"saveUser1", args{&model.User{-1, "test@gmail.com", "tester", "testerson", "patient", "password", patient}}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := SaveUser(tt.args.user); (err != nil) != tt.wantErr {
+				t.Errorf("SaveUser() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCreatePatient(t *testing.T) {
+	type args struct {
+		patient *model.Patient
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := CreatePatient(tt.args.patient); (err != nil) != tt.wantErr {
+				t.Errorf("CreatePatient() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}

5b8a325f007df96eb310a7c6e50bbb856c078788
Laurendy Lam <l.lam@sap.com>
2017-10-26 18:41:13 -0400

    #86 add popup support

--- client/src/typings.d.ts
+++ client/src/typings.d.ts
@@ -4,3 +4,25 @@ interface NodeModule {
   id: string;
   hot: any;
 }
+
+/**
+ * elementResizeDetectorMaker module
+ */
+declare function elementResizeDetectorMaker(options?: elementResizeDetectorMaker.ErdmOptions): elementResizeDetectorMaker.Erd;
+
+declare namespace elementResizeDetectorMaker {
+  interface ErdmOptions {
+    strategy?: 'scroll' | 'object';
+  }
+
+  interface Erd {
+    listenTo(element: HTMLElement, callback: (elem: HTMLElement) => void);
+    removeListener(element: HTMLElement, callback: (elem: HTMLElement) => void);
+    removeAllListeners(element: HTMLElement);
+    uninstall(element: HTMLElement);
+  }
+}
+
+declare module 'element-resize-detector' {
+  export default elementResizeDetectorMaker;
+}

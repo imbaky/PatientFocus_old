d15fe010c956c37f4937ffde42959511bffae6a3
Anhkhoi Vu-Nguyen <anhkhoi.vunguyen@gmail.com>
2017-10-27 19:10:50 -0400

    #117: Add selected document service

---
+++ client/src/modules/patient/shared/services/selected-document/selected-document.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { Document } from '../document/document.service';
+
+@Injectable()
+export class SelectedDocumentService {
+
+  /**
+   * List of documents
+   */
+  documents: Array<Document> = [];
+
+  /**
+   * Retrieve all documents
+   * @returns {Array<Documents>}
+   */
+  getAllDocuments(): Array<Document> {
+    return this.documents;
+  }
+
+  /**
+   * Add a document to existing documents
+   * @param {Document} document
+   */
+  addDocument(document: Document) {
+    if (this.documents.indexOf(document) === -1) {
+      this.documents.push(document);
+    }
+  }
+
+  /**
+   * Removes an existing document
+   * @param {Document} document
+   */
+  removeDocument(document: Document) {
+    const index = this.documents.indexOf(document);
+    this.documents.splice(index, 1);
+  }
+
+  /**
+   * Deletes all Document
+   */
+  clearAllDocuments() {
+    this.documents = [];
+  }
+
+  /**
+   * Checks if document already exists
+   * @param {Document} document
+   * @returns {boolean}
+   */
+  isSelected(document: Document): boolean {
+    return (this.documents.indexOf(document) !== -1);
+  }
+
+}

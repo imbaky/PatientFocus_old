fe77d2559a3bd9acc70a3af515f50eda555adc6b
Anhkhoi Vu-Nguyen <anhkhoi.vunguyen@gmail.com>
2017-10-29 20:52:07 -0400

    #137: Unit test

--- client/src/modules/patient/shared/services/patient/patient.service.spec.ts
+++ client/src/modules/patient/shared/services/patient/patient.service.spec.ts
@@ -10,6 +10,7 @@ const okResponse = { status: 200, statusText: 'OK' };
 describe('Document Service', () => {
 
   let patient: Patient;
+  let patients: Array<Patient>;
   let service: PatientService;
   let http: HttpClient;
   let store: Store;
@@ -40,6 +41,36 @@ describe('Document Service', () => {
     };
   });
 
+  patients = [
+    {
+      id: 1,
+      user_id: 1,
+      race: 'Canadian',
+      gender: 'male',
+      dob: '1996/08/19',
+      language: 'en_US',
+      smoke: false
+    },
+    {
+      id: 2,
+      user_id: 2,
+      race: 'Russian',
+      gender: 'male',
+      dob: '1946/08/19',
+      language: 'en_US',
+      smoke: false
+    },
+    {
+      id: 3,
+      user_id: 3,
+      race: 'Nigerian',
+      gender: 'male',
+      dob: '1969/09/11',
+      language: 'en_US',
+      smoke: false
+    },
+  ];
+
   it('GIVEN a patient id THEN the patient is returned.', inject([HttpTestingController], (httpMock: HttpTestingController) => {
     spyOn(store, 'set');
     service.getPatient(1)
@@ -54,6 +85,22 @@ describe('Document Service', () => {
 
     req.flush({ status: true , patient: patient }, okResponse);
     expect(store.set).toHaveBeenCalled();
+  }));
+
+  it('SHOULD get all patients', inject([HttpTestingController], (httpMock: HttpTestingController) => {
+
+    service.getAllPatients()
+      .subscribe((res: any) => {
+        expect(res.patients.length).toBe(3);
+        expect(res.patients[2].race).toBe('Nigerian');
+      });
+    const req = httpMock.expectOne( {
+      url: '/patients/',
+      method: 'GET'
+    });
+
+    req.flush({ status: true , patients: patients }, okResponse);
+
   }));
 
 });

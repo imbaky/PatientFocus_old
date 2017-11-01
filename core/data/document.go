1f14b476edcc26228b23064ebc0ba63bed8e8158
harrison <harrison.a101@hotmail.com>
2017-10-31 22:59:21 -0400

    #146 Query to insert received document into database and make association with patient.

--- core/data/document.go
+++ core/data/document.go
@@ -40,3 +40,24 @@ func GetDocumentsFromArray(docs []string, documents *[]model.Document) error {
 
     return nil
 }
+
+func InsertPatientDocument(document model.Document, patient model.Patient) error {
+    db, err := GetConnection()
+    if err != nil {
+        return fmt.Errorf("Db connection problem :%v", err)
+    }
+    insertDocumentQuery := `INSERT INTO document
+    (url,description,date_created,date_modified)
+    VALUES($1,$2, $3, $4) RETURNING id`
+    err = db.QueryRow(insertDocumentQuery, document.Url, document.Desc, document.Date_created, document.Date_modified).Scan(&document.Id)
+    if err != nil {
+        return fmt.Errorf("Error inserting document into database: %v", err)
+    }
+    associatePatientDocQuery := `INSERT INTO patient_document
+    (patient, document) Values($1,$2)`
+    _, err = db.Query(associatePatientDocQuery, patient.Id, document.Id)
+    if err != nil {
+        return fmt.Errorf("Error inserting associating patient and document in database: %v \n", err)
+    }
+    return nil
+}
--- core/domain/model/document.go
+++ core/domain/model/document.go
@@ -5,4 +5,6 @@ type Document struct {
 	Id        int    `json:"id"`
 	Url       string `json:"url"`  // path and filename
 	Desc      string `json:"description"` // short description of file (optional)
+	Date_created string
+	Date_modified string
 }

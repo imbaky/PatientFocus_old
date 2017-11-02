7c7e126eadbff581e747e48c2d82c7aaf95003e5
McGiver- <gkolampas@gmail.com>
2017-11-02 19:10:35 -0400

    migrated the models

---
+++ backend/domain/models/doctor.go
@@ -0,0 +1,11 @@
+package models
+
+//The Doctor struct holds all user data
+type Doctor struct {
+	Id int `json:"id"`
+}
+
+type DoctorDocument struct {
+	DoctorId   int `json:"doctor_id"`
+	DocumentId int `json:"document_id"`
+}

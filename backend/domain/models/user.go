7eb97748a81959422df04e0bac76d746e22b6116
McGiver- <gkolampas@gmail.com>
2017-11-03 10:33:40 -0400

    fixed some issues with the id names and the orm

--- backend/domain/models/user.go
+++ backend/domain/models/user.go
@@ -2,11 +2,11 @@ package models
 
 //The User struct holds all user data
 type PFUser struct {
-	Uid       int    `orm:"pk" json:"id"`
-	Email     string `json:"email"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Password  string `json:"password"`
+	Uid       int      `orm:"pk;auto" json:"id"`
+	Email     string   `json:"email"`
+	FirstName string   `json:"first_name"`
+	LastName  string   `json:"last_name"`
+	Password  string   `json:"password"`
 	Patient   *Patient `orm:"rel(fk);null"`
-	Doctor    *Doctor `orm:"rel(fk);null"`
+	Doctor    *Doctor  `orm:"rel(fk);null"`
 }

45221fbe375cb7676e94becedb1c600a74987c26
l3af <cyap.mobile@gmail.com>
2017-11-05 14:12:55 -0500

    Retrieve documents using ORM syntax

---
+++ backend/data/document.go
@@ -0,0 +1,25 @@
+package data
+
+import (
+    "fmt"
+    
+    "github.com/imbaky/PatientFocus/backend/domain/models"
+)
+
+func GetDocumentsFromArray(docs []int, documents *[]models.Document) error {
+
+	var dynamic string
+	for i := 0; i < len(docs); i++ {
+		dynamic += "?, "
+	}
+	dynamic = dynamic[:len(dynamic)-2]
+	query := fmt.Sprintf("SELECT did, url, desc FROM document WHERE did IN (%s)", dynamic)
+	num, err := ormObject.Raw(query, docs).QueryRows(*documents)
+	if err != nil {
+		return fmt.Errorf("Could not get documents: %v", err)
+	}
+	if int(num) != len(docs) {
+		return fmt.Errorf("Could not find all documents")
+	}
+	return nil
+}

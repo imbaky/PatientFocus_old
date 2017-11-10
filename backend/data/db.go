17c848eeeb6da76b02be886f4b8c90eacaff640c
McGyver- <gkolampas@gmail.com>
2017-11-09 20:18:36 -0500

    Update db.go

--- backend/data/db.go
+++ backend/data/db.go
@@ -37,9 +37,7 @@ func ConnectToDb() {
 		new(models.Patient),
 		new(models.Doctor),
 		new(models.Document),
-		// new(models.DoctorDocument),
 		new(models.Label),
-		// new(models.DocumentLabel),
 	)
 	orm.RunSyncdb("default", false, true)
 	ormObject = orm.NewOrm()

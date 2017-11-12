b29895bb4ae41e32f3a8437630977f80edb5f638
Anhkhoi Vu-Nguyen <anhkhoi.vunguyen@gmail.com>
2017-11-11 22:39:02 -0500

    #149: Add validator to email field within form with error message on submit

--- client/src/modules/patient/document/containers/document-sendform/document-send-form.component.ts
+++ client/src/modules/patient/document/containers/document-sendform/document-send-form.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { SelectedDocumentService } from '../../../shared/services/selected-document/selected-document.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ShareDocumentService } from '../../../shared/services/share-document/share-document.service';
+import { Document } from '../../../shared/services/document/document.service';
+
 
 @Component({
   selector: 'document-sendform',
@@ -9,23 +11,40 @@ import { ShareDocumentService } from '../../../shared/services/share-document/sh
 })
 export class DocumentSendFormComponent {
 
-  constructor(
-    private selectedDocumentService: SelectedDocumentService,
-    private fb: FormBuilder,
-    private shareDocumentService: ShareDocumentService
-  ) { }
-
-  form: FormGroup = this.fb.group({
-    'email': ['', [Validators.required]],
-    'message': ['', Validators.required ]
-  });
-
-  onSubmit() {
-    this.shareDocumentService.shareDocument(this.form.value.email, this.form.value.message, this.documents)
-      .subscribe();
+  constructor(private selectedDocumentService: SelectedDocumentService,
+              private fb: FormBuilder,
+              private shareDocumentService: ShareDocumentService) {
   }
 
-  get documents() {
+  /**
+   * Document send form
+   * @type {FormGroup}
+   */
+  form: FormGroup = this.fb.group({
+    'email': ['', [Validators.required, Validators.email]],
+    'message': ['']
+  });
+
+  errors = null;
+
+  /**
+   * Shares the documents when form is valid
+   */
+  onSubmit(): void {
+    if (this.form.valid) {
+      this.shareDocumentService.shareDocument(this.form.value.email, this.form.value.message, this.documents)
+        .subscribe();
+    } else {
+      this.form.get('email').markAsTouched();
+      this.form.get('email').updateValueAndValidity();
+    }
+  }
+
+  /**
+   * Retrieve all documents user has currently selected
+   * @returns {Array<Document>}
+   */
+  get documents(): Array<Document> {
     return this.selectedDocumentService.getAllDocuments();
   }
 
